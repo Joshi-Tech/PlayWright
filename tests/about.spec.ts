565f1818d63fe4772a95b65219a28ba16d93a9eb
Laxmi Joshi <laxmi.joshi@centre4.local>
2024-03-05 23:03:57 +0000

    removed commented code

--- tests/about.spec.ts
+++ tests/about.spec.ts
@@ -15,14 +15,14 @@ test.beforeEach(async ({ page }) => {
      page = await context.newPage()
     aboutPage = new AboutPage(page)
     await aboutPage.navigateTo(endPoint)          
-});
+})
 
 test('Page title can be seen as expected', async ({page})=>{
 expect(await aboutPage.getPageTitle()).toEqual('About | Automation Panda')
 })
 
 test('FOLLOW BLOG VIA EMAIL text can be seen', async ({page})=>{
-expect(await aboutPage.getFollowByEmailTxt()).toEqual(followByBlogTxt);
+expect(await aboutPage.getFollowByEmailTxt()).toEqual(followByBlogTxt)
 })
 
 test('About page has got all block headings', async ({page})=>{
@@ -32,6 +32,6 @@ expect(await aboutPage.getBlockHeading()).toStrictEqual(blockheading)
 
 test('User able to click on desired link', async ({page})=>{
 console.log(await aboutPage.getBlockHeading())
-console.log(await page.title());
+console.log(await page.title())
 })
 })
